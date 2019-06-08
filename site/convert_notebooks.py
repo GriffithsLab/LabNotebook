697873f015c8886744385411d824b11de3d1e981
John Griffiths <j.davidgriffiths@gmail.com>
2019-06-08 14:13:17 -0400

    fixed conversion

--- site/convert_notebooks.py
+++ site/convert_notebooks.py
@@ -1,4 +1,4 @@
-import os,glob
+import os,glob,nbformat
 
 nbc_str_tpl = 'jupyter nbconvert %s --to markdown --NbConvertApp.output_files_dir=.'
 
@@ -45,7 +45,7 @@ index_txt = \
 """
 
 +++
-title = "Title %s"  # Add a page title.
+title = "%s"  # Add a page title.
 summary = "Hello!"  # Add a page description.
 date = 2019-01-01T00:00:00  # Add today's date.
 type = "widget_page"  # Page type is a Widget Page
@@ -63,36 +63,38 @@ print('notebooks: %s' %nbs)
 
 for nb_it,nb in enumerate(nbs):
 
-    nb_full = nb_dir + '/' + nb
-    
+    nb_full = nb_dir + '/' + nb    
     nb_name = nb.replace('.ipynb', '')
+    
+    F = open(nb_full, 'r')
+    thisnb = nbformat.read(F,nbformat.NO_CONVERT)
+    nb_md = thisnb['metadata']
+    
+    if 'grifflab_labnotebook' in nb_md:
+        gl_nb_md = nb_md['grifflab_labnotebook']
+        post_title = gl_nb_md['post_title']    
         
-    new_dir = entries_dir + '/%s' % nb_name
-    print('going to new dir: %s' %new_dir)
-    if not os.path.isdir(new_dir): os.makedirs(new_dir)
-    os.chdir(new_dir)
+        new_dir = entries_dir + '/%s' % nb_name
+        print('going to new dir: %s' %new_dir)
+        if not os.path.isdir(new_dir): os.makedirs(new_dir)
+        os.chdir(new_dir)
+        print('copying file %s to .' %nb_full)
+        os.system('cp %s .'  %nb_full)
+        print('running nbconvert for %s' %nb)
+        nbc_str = nbc_str_tpl % nb 
+        os.system(nbc_str)
     
-    print('copying file %s to .' %nb_full)
-    os.system('cp %s .'  %nb_full)
+        md_file = nb_name + '.md'
+        md_str = top_lines + open(md_file, 'r').read()
+        open(md_file, 'w+').write(md_str)
 
-    if os.path.isfile(nb):
+        index_file = 'index.md'
+        open(index_file, 'w+').write(index_txt %post_title)    
+  
+        os.system('rm %s' %nb)
 
-      print('running nbconvert for %s' %nb)
-
-      nbc_str = nbc_str_tpl % nb 
-      os.system(nbc_str)
-   
-    
-      md_file = nb_name + '.md'
-      md_str = top_lines + open(md_file, 'r').read()
-      open(md_file, 'w+').write(md_str)
-
-      index_file = 'index.md'
-      open(index_file, 'w+').write(index_txt %nb_it)    
-   
     else:
-
-      print('%s - file not found' %nb)
+        print('%s - skipping; grifflab labnotebook info not in metadata' %nb)
 
 os.chdir(base_dir)
 
