8c8aa1493877bf11641bb8232fc690a586c5f3c2
John Griffiths <j.davidgriffiths@gmail.com>
2019-10-22 13:01:40 -0400

    add math params

--- site/convert_notebooks.py
+++ site/convert_notebooks.py
@@ -49,6 +49,9 @@ title = "%s"  # Add a page title.
 summary = "Hello!"  # Add a page description.
 date = 2019-01-01T00:00:00  # Add today's date.
 type = "widget_page"  # Page type is a Widget Page
+math = true
+diagram = true
+markup = "mmark"
 +++
 
 """
